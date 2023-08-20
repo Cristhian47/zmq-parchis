193bf5048ce08b5a216aea082008eaff3f46d10d
dospolas <esteban.herreraa.eha@gmail.com>
2023-08-20 12:33:34 -0500

    Se arreglaron los llamados a client y se trabajo en la lanzada de dados

--- CLIENTS/player_menu.py
+++ CLIENTS/player_menu.py
@@ -1,6 +1,31 @@
 import zmq
 from player_connection import connection
 
+def lanzar_dados(self):
+        input("Presiona Enter para lanzar los dados...")
+        dado1 = random.randint(1, 6)
+        dado2 = random.randint(1, 6)
+        print(f"Primer dado: {dado1}")
+        print(f"Segundo dado: {dado2}")
+
+        print("Elige cómo asignar los dados a las fichas:")
+        print("1. Dado 1 a ficha y Dado 2 a otra ficha")
+        print("2. Suma de los dados a una ficha")
+
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            ficha1 = int(input("Selecciona el id de la primera ficha (1, 2, 3 o 4): "))
+            ficha2 = int(input("Selecciona el id de la segunda ficha (1, 2, 3 o 4): "))
+            print(f"Dado 1 asignado a ficha {ficha1}")
+            print(f"Dado 2 asignado a ficha {ficha2}")
+        elif opcion == "2":
+            ficha = int(input("Selecciona el id de la ficha (1, 2, 3 o 4): "))
+            suma_dados = dado1 + dado2
+            print(f"Suma de los dados asignada a ficha {ficha}: {suma_dados}")
+        else:
+            print("Opción inválida")
+
 def principal_menu(socket):
     while True:
             print("\nOptions :")
